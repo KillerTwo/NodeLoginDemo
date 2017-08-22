9ffdfcd37477bdc8ea4fe7b3a3f47a27e45a257a
KillerTwo <lwt2710@163.com>
2017-08-22 13:08:37 +0800

    NodeLogin
    nodejs login NoSql

---
+++ server.js
@@ -0,0 +1,16 @@
+/*
+* @Author: KillerTwo
+* @Date:   2017-08-22 13:04:29
+* @Last Modified by:   KillerTwo
+* @Last Modified time: 2017-08-22 13:08:27
+*/
+
+var http = require('http');
+
+var fs= require('fs');
+
+var url = require('url');
+
+
+
+
